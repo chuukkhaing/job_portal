461ff1c0a5b73deea7e333e33d46101231a3befb
chuukkhaing <chuukkhaing96@gmail.com>
2023-07-10 16:46:01 +0630

    job-post

--- resources/views/seeker/profile/profile-dashboard.blade.php
+++ resources/views/seeker/profile/profile-dashboard.blade.php
@@ -108,10 +108,13 @@
                             @if($jobPost->country == 'Other') 
                             <span class="jobpost-attr">Other Country</span>
                             @endif
-                            @if($jobPost->salary_status)
-                            <p style="color: #181722">{{ $jobPost->salary_status }}</p>
-                            @elseif($jobPost->salary_range)
+                            @if($jobPost->salary_status == 'Negotiable')
+                            <p style="color: #181722">Negotiate</p>
+                            @endif
+                            @if($jobPost->salary_status != 'Hide' && $jobPost->salary_status != 'Negotiable')
+                            @if($jobPost->salary_range)
                             <p style="color: #181722">{{ $jobPost->salary_range }} {{ $jobPost->currency }}</p>
+                            @endif
                             @endif
                         </div>
                         <div class="col-2 d-flex align-items-end flex-column bd-highlight mb-3">
