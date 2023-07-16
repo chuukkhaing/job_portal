079548fb408cb06433707342f5dfb64fd198d948
CKK-8xpand <chuu.kaykhiang@8xpand.com>
2023-07-17 02:00:52 +0630

    applicant-detail

--- app/Http/Controllers/Employer/EmployerJobPostController.php
+++ app/Http/Controllers/Employer/EmployerJobPostController.php
@@ -142,18 +142,21 @@ class EmployerJobPostController extends Controller
         $seeker = Seeker::findOrFail($jobApply->first()->seeker_id);
         $educations = SeekerEducation::whereSeekerId($seeker->id)->get();
         $experiences = DB::table('seeker_experiences as a')
+                        ->where('a.seeker_id','=',$seeker->id)
                         ->join('industries as b','a.industry_id','=','b.id')
                         ->join('functional_areas as c','a.main_functional_area_id','=','c.id')
                         ->join('functional_areas as d','a.sub_functional_area_id','=','d.id')
                         ->select('a.*','b.name as industry_name', 'c.name as main_functional_area_name', 'd.name as sub_functional_area_name')
                         ->get();
         $skill_main_functional_areas = DB::table('seeker_skills as a')
+                        ->where('a.seeker_id','=',$seeker->id)
                         ->join('skills as b','a.skill_id','=','b.id')
                         ->join('functional_areas as c','a.main_functional_area_id','=','c.id')
                         ->select('a.*', 'b.name as skill_name', 'c.name as main_functional_area_name')
                         ->groupBy('a.main_functional_area_id')
                         ->get();
         $skills = DB::table('seeker_skills as a')
+                    ->where('a.seeker_id','=',$seeker->id)
                     ->join('skills as b','a.skill_id','=','b.id')
                     ->join('functional_areas as c','a.main_functional_area_id','=','c.id')
                     ->select('a.*', 'b.name as skill_name', 'c.name as main_functional_area_name')
