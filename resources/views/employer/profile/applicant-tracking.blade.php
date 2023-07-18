2d3f2c53e154ed97e0b645dcbd0b273db69b5462
chuukkhaing <chuukkhaing96@gmail.com>
2023-07-18 14:31:06 +0630

    cv-attach

--- resources/views/employer/profile/applicant-tracking.blade.php
+++ resources/views/employer/profile/applicant-tracking.blade.php
@@ -270,7 +270,7 @@
                     if(value.seeker_id == response.seeker.id) {
                         active = 'active'
                     }
-                    $(".applicant-receive-table").append('<tr class="applicant-receive-table-tr"><td class="'+active+'" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')">'+value.seeker_first_name+' '+value.seeker_last_name+'</td><td class="text-end" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')">'+moment(value.seeker_applied_date).format("DD/MM/YYYY")+'</td></tr>');
+                    $(".applicant-receive-table").append('<tr class="applicant-receive-table-tr" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')"><td class="'+active+'">'+value.seeker_first_name+' '+value.seeker_last_name+'</td><td class="text-end">'+moment(value.seeker_applied_date).format("DD/MM/YYYY")+'</td></tr>');
                     $(".download_seeker_cv").attr('href',document.location.origin+'/storage/seeker/cv/'+response.seeker_attach.name);
                     $(".download_ic_cv").attr('href', document.location.origin+'/employer/download-ic-cv/'+response.seeker.id)
                     if(response.seeker.gender == 'Female') {
@@ -376,6 +376,7 @@
             url: 'get-jobpost-info/'+id+'/'+jobPostId,
         }).done(function(response){
             if(response.status == 'success') {
+                console.log('true')
                 $("#receive-cv-length").text(response.jobApply.length);
                 $("#receive-job-title").text(response.jobPost.job_title);
                 $(".applicant-receive-table-tr").remove();
@@ -386,7 +387,7 @@
                     if(value.seeker_id == response.seeker.id) {
                         active = 'active'
                     }
-                    $(".applicant-receive-table").append('<tr class="applicant-receive-table-tr"><td class="'+active+'" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')">'+value.seeker_first_name+' '+value.seeker_last_name+'</td><td class="text-end" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')">'+moment(value.seeker_applied_date).format("DD/MM/YYYY")+'</td></tr>');
+                    $(".applicant-receive-table").append('<tr class="applicant-receive-table-tr" onclick="getRelatedApplicantInfo('+value.seeker_id+','+value.job_post_id+')"><td class="'+active+'">'+value.seeker_first_name+' '+value.seeker_last_name+'</td><td class="text-end">'+moment(value.seeker_applied_date).format("DD/MM/YYYY")+'</td></tr>');
                     $(".download_seeker_cv").attr('href',document.location.origin+'/storage/seeker/cv/'+response.seeker_attach.name);
                     $(".download_ic_cv").attr('href', document.location.origin+'/employer/download-ic-cv/'+response.seeker.id)
                     
