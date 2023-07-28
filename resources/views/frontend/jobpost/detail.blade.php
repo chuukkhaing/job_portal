761c1fe6d2b1c9df01c0b123a38a39707d619c23
khinbmoe-thu <k.thuthu90@gmail.com>
2023-07-28 12:23:48 +0630

    add: job post detail

--- resources/views/frontend/jobpost/detail.blade.php
+++ resources/views/frontend/jobpost/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.layouts.app')
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row">
         <div class="col-12 col-md-5 my-5">
             <div class="ex3">
@@ -170,5 +170,215 @@
             </div>
         </div>
     </div>
+</div> --}}
+
+<!-- Banner Start -->
+<div class="container-fluid p-3">
+    <div class="row company-detail-banner">
+        <img src="{{ asset('/frontend/img/company/company-banner-image.png') }}" class="w-100" alt=" }}">
+    </div>
 </div>
+<!-- Banner End -->
+
+<!-- Job Post Profile Start -->
+<div class="container">
+    <div class="row pt-3 px-3">
+        <div class="col-lg-6 col-md-6 col-6">
+            <img src="{{ asset('frontend/img/company/profile-image.png') }}" class="" style="width: 120px; height: 120px" alt="">
+
+            <div class="company-name pt-4 pb-2">
+                <h3>Senior Java Lead Developer</h3>
+                <span>Spring Company</span>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-6 align-self-end">
+            <div class="float-end pb-2">
+                <a href="http://" class="btn apply-company-btn py-2">
+                    <i class="fa-solid fa-arrow-right"></i> <span class="p-1">Apply on company site</span>
+                </a>
+                <a href="http://" class="btn btn-outline-primary py-2">
+                    <i class="fa fa-heart-o p-1 fw-bold"></i><span class="p-1">Save Job</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-3 px-3">
+        <div class="col-12">
+            <div class="company-name pt-4 pb-2">
+                <span>Yangon , Myanmar</span>
+                <h3>1 Lakhs - 10 Lakhs  - Full-time</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-3 px-3">
+        <div class="col-lg-2 col-md-3 col-6">
+            <div class="company-name pt-4 pb-2">
+                <h3>Career Level</h3>
+                <span>Senior Manager</span>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-6">
+            <div class="company-name pt-4 pb-2">
+                <h3>Years of Experience</h3>
+                <span>12 years</span>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-6">
+            <div class="company-name pt-4 pb-2">
+                <h3>Job Specializations</h3>
+                <span>Computer/Information Technology, IT-Network</span>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-6">
+            <div class="company-name pt-4 pb-2">
+                <h3>Qualification</h3>
+                <span>Not Specified</span>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-6">
+            <div class="company-name pt-4 pb-2">
+                <h3>Job Type</h3>
+                <span>Full-Time/Part-Time/Project-Base</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Job Post Profile End -->
+
+<div class="container">
+    <div class="row col-12 m-0 p-0 mt-5">
+        <!-- Job Post Detail Start-->
+        <div class="col-lg-7 col-12">
+            <div class="row col-12 m-0 p-0 py-3">
+                <h5 class="fw-bolder fs-6">Job Description</h5>
+                <p>
+                    We are seeking a self-motivated and innovative Merchant Engagement Specialist to join our Sale & Merchant Engagement Business Unit and help us build strong relationships with our Payroll Customer /KBZPay merchants. You will be part of a team that specializes in merchant engagement and merchant care. The job requires you to provide after-sales service to merchants and ensure merchants have the necessary POSM materials and have full knowledge to operate the Payroll product and KBZPay partner app. In addition you will be part of the team that is the primary interface to merchants to solve their issues, drive superior Payroll customers and Merchant satisfaction. To excel in this role, the individual needs to have both outstanding interpersonal skills and ability to build positive relationships or alliances. In addition, experiences in customer service will be essential.
+                    Be part of a team to engage merchants for both Payroll customers and KBZPay merchants.
+                    Provide product knowledge of Payroll for Customers awareness.
+                    Provide required product (KBZPay partner app, portal) trainings to merchants and respective users at outlets.
+                    Ensure after sale activities to address any gaps left in both pre-sales and sales activities.
+                    Execute merchants’ segmentation and engagement strategies to create win-win outcomes for our customers, the bank and the merchants.
+                    Provide Level 1 support to address merchant issues related to the use of Payroll product or KBZPay partner app.
+                    Be a focal point of contact for merchants and payroll customers to resolve issues.
+                    Actively collaborate with cross-functional departments to ensure merchants and payroll customers issues and requirements are resolved within the SLA.
+                </p>
+            </div>
+
+            <div class="row col-12 m-0 p-0 py-3">
+                <h5 class="fw-bolder fs-6">Job Requirements</h5>          
+                <div>
+                    <ul>
+                        <li>Possess a bachelor degree (Degree in Banking/Business Administration)</li>
+                        <li>Proficient in English language</li>
+                        <li>Excellent customer service skill and communication skill</li>
+                        <li>Strong organizational and influential skills</li>
+                        <li>Strong Teamwork and excellent interpersonal skills</li>
+                        <li>Skilled in oratory and presentation</li>
+                        <li>Willing to work overtime, when needed, to complete the jobs in time and for good results</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row col-12 m-0 p-0 py-3">
+                <h5 class="fw-bolder fs-6">We expect you to have:</h5>          
+                <div>
+                    <ul>
+                        <li>Possess a bachelor degree (Degree in Banking/Business Administration)</li>
+                        <li>Proficient in English language</li>
+                        <li>Excellent customer service skill and communication skill</li>
+                        <li>Strong organizational and influential skills</li>
+                        <li>Strong Teamwork and excellent interpersonal skills</li>
+                        <li>Skilled in oratory and presentation</li>
+                        <li>Willing to work overtime, when needed, to complete the jobs in time and for good results</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Job Post Detail End-->
+
+        <!-- Similar Jobs Start-->
+        <div class="col-lg-5 col-12">
+            <div class="px-3 m-0 pb-0 pt-3">
+                <h5 class="text-blue fw-bolder">More Similar Jobs</h5>
+            </div>
+        
+            <div class="row m-0 pb-0">
+                <div class="col-12">
+                    <div class="m-0 pb-0 border-bottom">
+                        <div class="row p-0 m-0">
+                            <div class="col-lg-10 col-md-10 py-4 px-1">
+                                <div class="row m-0">
+                                    <div class="col-lg-2 col-12 job-image p-0 px-1">
+                                        <img src="http://localhost:93/frontend/img/trending/aya.png" class="img-responsive center-block d-block mx-auto" alt="Job Profile">
+                                    </div>
+        
+                                    <div class="col-lg-10 col-12">
+                                        <div class="job-company">Blackzim</div>
+                                        <div class="job-title">Senior Java Developer</div>
+                                        <div class="job-location">Yangon , Myanmar</div>
+                                        <div class="job-salary my-3">1 Lakhs - 10 Lakhs</div>
+                                    </div>
+                                </div>
+                            </div>
+        
+                            <div class="col-lg-2 col-md-2 d-flex align-items-end flex-column bd-highlight py-4 px-1">
+                                <div class="row col-12 m-0 p-0">
+                                    <div class="text-end px-0">
+                                        <i class="fa fa-heart-o text-blue"></i>
+                                    </div>
+            
+                                    <div class="text-end mt-auto  px-0">
+                                        <span>1 d</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="m-0 pb-0 border-bottom">
+                        <div class="row p-0 m-0">
+                            <div class="col-lg-10 col-md-10 py-4 px-1">
+                                <div class="row m-0">
+                                    <div class="col-lg-2 col-12 job-image p-0 px-1">
+                                        <img src="http://localhost:93/frontend/img/trending/aya.png" class="img-responsive center-block d-block mx-auto" alt="Job Profile">
+                                    </div>
+        
+                                    <div class="col-lg-10 col-12">
+                                        <div class="job-company">Blackzim</div>
+                                        <div class="job-title">Senior Java Developer</div>
+                                        <div class="job-location">Yangon , Myanmar</div>
+                                        <div class="job-salary my-3">1 Lakhs - 10 Lakhs</div>
+                                    </div>
+                                </div>
+                            </div>
+        
+                            <div class="col-lg-2 col-md-2 d-flex align-items-end flex-column bd-highlight py-4 px-1">
+                                <div class="row col-12 m-0 p-0">
+                                    <div class="text-end px-0">
+                                        <i class="fa fa-heart-o text-blue"></i>
+                                    </div>
+            
+                                    <div class="text-end mt-auto px-0">
+                                        <span>1 d</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Similar Jobs End-->
+    </div>
+</div>
+
 @endsection
