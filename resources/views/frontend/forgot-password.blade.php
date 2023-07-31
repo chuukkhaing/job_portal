ff0595d99a2c6bf86e8af4453bb934fcf7dbe7ee
khinbmoe-thu <k.thuthu90@gmail.com>
2023-07-31 12:45:35 +0630

    add: employer reset password

--- resources/views/frontend/forgot-password.blade.php
+++ resources/views/frontend/forgot-password.blade.php
@@ -1,21 +1,40 @@
 @extends('frontend.layouts.app')
 @section('content')
 
+@php
+    $route_name = Route::currentRouteName();
+@endphp
+
 <div class="container">
     <div class="py-3">
         <article class="mx-auto">
-            <form action="{{ route('seeker-forgot.post') }}" method="post">
-                @csrf
-                <div class="form-group input-group register-form-input p-2 my-3">
-                    <div class="input-group-prepend d-flex">
-                        <span class="input-group-text border-0 bg-transparent"> <i class="fa fa-envelope"></i> </span>
+            @if($route_name == 'seeker-forgot')
+                <form action="{{ route('seeker-forgot.post') }}" method="post">
+                    @csrf
+                    <div class="form-group input-group register-form-input p-2 my-3">
+                        <div class="input-group-prepend d-flex">
+                            <span class="input-group-text border-0 bg-transparent"> <i class="fa fa-envelope"></i> </span>
+                        </div>
+                        <input name="email" class="form-control border-0" placeholder="Enter Email" type="email" value="{{ old('email') }}" required>
                     </div>
-                    <input name="email" class="form-control border-0" placeholder="Enter Email" type="email" value="{{ old('email') }}" required>
-                </div>
-                <div class="form-group p-2">
-                    <button type="submit" class="btn col-12 btn-signup"> Reset Password  </button>
-                </div>                                                             
-            </form>
+                    <div class="form-group p-2">
+                        <button type="submit" class="btn col-12 btn-signup"> Reset Password  </button>
+                    </div>                                                             
+                </form>
+            @else
+                <form action="{{ route('employer-forgot.post') }}" method="post">
+                    @csrf
+                    <div class="form-group input-group register-form-input p-2 my-3">
+                        <div class="input-group-prepend d-flex">
+                            <span class="input-group-text border-0 bg-transparent"> <i class="fa fa-envelope"></i> </span>
+                        </div>
+                        <input name="company_email" class="form-control border-0" placeholder="Enter Email" type="email" value="{{ old('company_email') }}" required>
+                    </div>
+                    <div class="form-group p-2">
+                        <button type="submit" class="btn col-12 btn-signup"> Reset Password  </button>
+                    </div>                                                             
+                </form>
+            @endif
         </article>
     </div>
 </div>
