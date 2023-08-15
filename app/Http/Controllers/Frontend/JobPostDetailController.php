3d41f75b83bdb8ae055fc255f999e4fee5624243
chuukkhaing <chuukkhaing96@gmail.com>
2023-08-15 09:22:01 +0630

    DB

--- app/Http/Controllers/Frontend/JobPostDetailController.php
+++ app/Http/Controllers/Frontend/JobPostDetailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Employer\JobPost;
 use App\Models\Admin\Package;
+use DB;
 
 class JobPostDetailController extends Controller
 {
