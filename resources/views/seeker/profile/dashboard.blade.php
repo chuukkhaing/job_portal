929ce83dfbf4b2410e1e1ba3a3265d7cec9c7e17
chuukkhaing <chuukkhaing96@gmail.com>
2023-11-28 13:27:40 +0630

    loading-test

--- resources/views/seeker/profile/dashboard.blade.php
+++ resources/views/seeker/profile/dashboard.blade.php
@@ -10,13 +10,13 @@
                     <div class="col-12">
                         <div class="row pb-3">
                             <div class="col-1">
-                                {{dd('test')}}
                                 @if(Auth::guard('seeker')->user()->image)
                                 <img src="{{ getS3File('seeker/profile/'.Auth::guard('seeker')->user()->id ,Auth::guard('seeker')->user()->image) }}" alt="Profile Image" class="img-thumbnail resume_profile_img rounded-circle" id="resume_profile_img">
                                 @else
                                 <img src="https://placehold.jp/200x200.png" alt="Profile Image" class="img-thumbnail resume_profile_img rounded-circle" id="resume_profile_img">
                                 @endif
                             </div>
+                            {{dd('test')}}
                             <div class="col-8 p-0 m-0 align-self-center">
                                 <div class="col-12 m-0">
                                     <div class="seeker-name">{{ Auth::guard('seeker')->user()->first_name }} {{ Auth::guard('seeker')->user()->last_name }}</div>
