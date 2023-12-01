6cf0abaddb2945883201158e065feab8e916a272
chuukkhaing <chuukkhaing96@gmail.com>
2023-12-01 10:48:53 +0630

    member-job

--- app/Http/Controllers/Frontend/CompanyDetailController.php
+++ app/Http/Controllers/Frontend/CompanyDetailController.php
@@ -13,7 +13,15 @@ class CompanyDetailController extends Controller
     public function companyDetail($slug)
     {
         $employer = Employer::whereSlug($slug)->first();
-        $jobPosts = JobPost::whereEmployerId($employer->id)->where('is_active',1)->where('status','Online')->where('hide_company', 0)->orderBy(DB::raw('FIELD(job_post_type, "feature", "trending")'),'desc')->get()->take(6);
+        $member_ids = $employer->Member->pluck('id')->toArray();
+        $employer_id = [];
+        foreach($member_ids as $member_id) {
+            $employer_id[] = $member_id;
+        }
+        
+        $employer_id[] = $employer->id;
+        $employer_id[] = $employer->employer_id;
+        $jobPosts = JobPost::whereIn('employer_id', $employer_id)->where('is_active',1)->where('status','Online')->where('hide_company', 0)->orderBy(DB::raw('FIELD(job_post_type, "feature", "trending")'),'desc')->get()->take(6);
         $packages = Package::whereNull('deleted_at')->where('is_active',1)->get();
         if($employer->EmployerMedia->where('type','Video Link')->count() > 0) {
             $videourl = $employer->EmployerMedia->where('type','Video Link')->first()->name;
