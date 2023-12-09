1ceb241981d7e6c26e7c3b205c39e0af318a8f5b
chuukkhaing <chuukkhaing96@gmail.com>
2023-12-09 23:40:26 +0630

    email-template

--- resources/views/seeker/verify/verifyEmail.blade.php
+++ resources/views/seeker/verify/verifyEmail.blade.php
@@ -5,46 +5,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background-image : url({{public_path('/img/background/email_bg.png')}}); background-position: center; background-repeat: no-repeat; background-size: contain;">
-    
-    <div class="container" style="width: 100%; margin: auto;">
-        <div class="row justify-content-center">
-            <div class="col-md-8 verify-email m-auto" style="text-align: center; padding: 80px 0;">
-                <div class="card">
-                    <div class="card-header">
-                        <div>
-                            
-                        </div>
+    <body>
+        <div style="width: 100%;">
+            <table width="100%" border="0" cellspacing="0" cellpadding="20" background="https://infinitycareers.com.mm/img/background/email_bg.png" style="text-align: center; background-position: center; background-repeat: no-repeat; background-size: contain;">
+                <tr>
+                    <td>
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="https://infinitycareers.com.mm/img/background/email_top_image.png" alt="" style="width: 150px; padding: 100px 0 0 30px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         <h2 style="font-weight: 900; color: #0355D0">ACTIVATE</h2>
                         <h4>Your Infinity Careers Account</h4>
-                    </div>
-                    <div class="card-body">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         <p style="color: #0355D0; font-weight: 600;">Hi {{ $seeker->email }},</p>
                         <p style="color: #0355D0; font-weight: 600;">Welcome to Infinity Careers - Where Opportunities Await!</p>
                         <p style="color: #0355D0; font-weight: 600;">To start your job search journey, simply click <br> the button below to activate your account:</p>
-
-                        <div style="text-align: center;">
-                            
-                        </div>
-                        
-                        <div style="margin: 20px 0;">
-                            <a href="{{ route('seeker-verify',$seeker->email_verification_token) }}" style="text-decoration: none; padding: 10px 50px; background: #0355D0; color: #FFD200; border-radius: 50px; box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.2); font-weight: bold;">Activate</a>
-                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="https://infinitycareers.com.mm/img/background/email_middle_image.png" alt="" style="width: 150px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="{{ route('seeker-verify',$seeker->email_verification_token) }}" style="text-decoration: none; padding: 10px 50px; background: #0355D0; color: #FFD200; border-radius: 50px; box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.2); font-weight: bold;">Activate</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         <p style="color: #0355D0; font-weight: 600;">With Infinity Careers, you'll discover exciting job opportunities and resources <br> to boost your career. We're here to help you succeed.</p>
                         <p style="color: #0355D0; font-weight: 600;">If you have any questions, our support team is ready to assist you.</p>
-                    </div>
-                    <div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         <p style="color: #0355D0; font-weight: 600;">Thanks for choosing us!</p>
-                    </div>
-                    <div class="card-footer" style="position: absolute; left: 45%;">
-                        
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         <p style="color: #fff; font-weight: 600;">Best Regards,</p>
                         <p style="color: #fff; font-weight: 600;">The Infinity Careers Team</p>
-                    </div>
-                </div>
-            </div>
+                    </td>
+                </tr>
+            </table>
         </div>
-    </div>
 
-</body>
+    </body>
 </html>
