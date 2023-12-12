d9a8aaf1e22c7c16a1aa6992a2fd014605a30023
chuukkhaing <chuukkhaing96@gmail.com>
2023-12-12 16:45:14 +0630

    edu-crud

--- app/Http/Controllers/API/Seeker/SeekerEducationController.php
+++ app/Http/Controllers/API/Seeker/SeekerEducationController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\API\Seeker;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Seeker\SeekerEducation;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Seeker\Seeker;
+use App\Models\Seeker\SeekerPercentage;
 
 class SeekerEducationController extends Controller
 {
@@ -15,7 +18,7 @@ class SeekerEducationController extends Controller
      */
     public function index(Request $request)
     {
-        $educations           = SeekerEducation::whereSeekerId($request->user()->id)->get();
+        $educations           = SeekerEducation::whereSeekerId($request->user()->id)->select('id','seeker_id','degree','major_subject','location','from','to','school','is_current')->get();
         
         return response()->json([
             'status' => 'success',
@@ -41,7 +44,54 @@ class SeekerEducationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator =  Validator::make($request->all(), [
+            'degree'           => ['required'],
+            'school'           => ['required'],
+            'major_subject'    => ['required'],
+            'location'         => ['required'],
+            'school'           => ['required'],
+            'from'             => ['numeric','required'],
+            'is_current'       => ['required'],
+            'to'               => ['numeric','required_if:is_current,0', 'gte:from']
+        ]);
+        if ($validator->fails()) {
+            return response(['errors'=>$validator->messages()], 422);
+        }else {
+            if($request->is_current == 1) {
+                $request->to = null;
+            }
+            $education_create = SeekerEducation::create([
+                'seeker_id'     => $request->user()->id,
+                'degree'        => $request->degree,
+                'major_subject' => $request->major_subject,
+                'location'      => $request->location,
+                'from'          => $request->from,
+                'to'            => $request->to,
+                'school'        => $request->school,
+                'is_current'    => $request->is_current ?? 0
+            ]);
+
+            $seeker            = Seeker::findOrFail($request->user()->id);
+            $seeker_educations = SeekerEducation::whereSeekerId($request->user()->id)->get();
+            if ($seeker_educations->count() > 0) {
+                $seeker_percent        = SeekerPercentage::whereSeekerId($request->user()->id)->whereTitle('Education')->first();
+                $seeker_percent_update = $seeker_percent->update([
+                    'percentage' => 10,
+                ]);
+                $total_percent = SeekerPercentage::whereSeekerId($request->user()->id)->sum('percentage');
+                $seeker_update = $seeker->update([
+                    'percentage' => $total_percent,
+                ]);
+            }
+
+            $education           = SeekerEducation::whereSeekerId($request->user()->id)->select('id','seeker_id','degree','major_subject','location','from','to','school','is_current')->findOrFail($education_create->id);
+
+            return response()->json([
+                'status'    => 'success',
+                'education' => $education,
+                'msg'       => 'Education Create successfully!',
+            ], 200);
+        }
     }
 
     /**
@@ -75,7 +125,42 @@ class SeekerEducationController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator =  Validator::make($request->all(), [
+            'degree'           => ['required'],
+            'school'           => ['required'],
+            'major_subject'    => ['required'],
+            'location'         => ['required'],
+            'school'           => ['required'],
+            'from'             => ['numeric','required'],
+            'is_current'       => ['required'],
+            'to'               => ['numeric','required_if:is_current,0', 'gte:from']
+        ]);
+        if ($validator->fails()) {
+            return response(['errors'=>$validator->messages()], 422);
+        }else {
+            if($request->is_current == 1) {
+                $request->to = null;
+            }
+            $education = SeekerEducation::findOrFail($id);
+            $education_update = $education->update([
+                'seeker_id'     => $request->user()->id,
+                'degree'        => $request->degree,
+                'major_subject' => $request->major_subject,
+                'location'      => $request->location,
+                'from'          => $request->from,
+                'to'            => $request->to,
+                'school'        => $request->school,
+                'is_current'    => $request->is_current ?? 0
+            ]);
+
+            $education           = SeekerEducation::whereSeekerId($request->user()->id)->select('id','seeker_id','degree','major_subject','location','from','to','school','is_current')->findOrFail($id);
+
+            return response()->json([
+                'status'    => 'success',
+                'education' => $education,
+                'msg'       => 'Education Update successfully!',
+            ], 200);
+        }
     }
 
     /**
@@ -84,8 +169,26 @@ class SeekerEducationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id, Request $request)
     {
-        //
+        $education               = SeekerEducation::findOrFail($id)->delete();
+        $seeker                  = Seeker::findOrFail($request->user()->id);
+        $seeker_educations_count = SeekerEducation::whereSeekerId($request->user()->id)->count();
+        if ($seeker_educations_count == 0) {
+            $seeker_percent        = SeekerPercentage::whereSeekerId($request->user()->id)->whereTitle('Education')->first();
+            $seeker_percent_update = $seeker_percent->update([
+                'percentage' => 0,
+            ]);
+            $total_percent = SeekerPercentage::whereSeekerId($request->user()->id)->sum('percentage');
+            $seeker_update = $seeker->update([
+                'percentage' => $total_percent,
+            ]);
+        }
+
+        return response()->json([
+            'status'                  => 'success',
+            'msg'                     => 'Education deleted successfully!',
+            'seeker_educations_count' => $seeker_educations_count,
+        ]);
     }
 }
