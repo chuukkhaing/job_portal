40f87df9f609dc5979f1f5037f7a9993fde9a49f
chuukkhaing <chuukkhaing96@gmail.com>
2023-12-15 11:44:13 +0630

    cover

--- app/Http/Controllers/Seeker/ResumeController.php
+++ app/Http/Controllers/Seeker/ResumeController.php
@@ -229,7 +229,7 @@ class ResumeController extends Controller
         $result = $client->completions()->create([
             'prompt' => 'Write about my cover letter. My name : ' . $seeker->first_name . $seeker->last_name . '.' . $my_exp . '.' . $my_edu. '.' . $my_skill,
             'model' => 'text-davinci-002',
-            'max_tokens' => 250,
+            'max_tokens' => 250, 
         ]);
 
         return response()->json([
