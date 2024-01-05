85f998917682e2c19d7d6059afd392d709a700c0
chuukkhaing <chuukkhaing96@gmail.com>
2024-01-05 16:34:18 +0630

    ownership_type

--- app/Http/Controllers/API/Employer/EmployerProfileController.php
+++ app/Http/Controllers/API/Employer/EmployerProfileController.php
@@ -82,13 +82,13 @@ class EmployerProfileController extends Controller
             }])->select('id', 'name', 'is_active')->where('is_active', 1)->whereNull('deleted_at');
         }, 'EmployerAddress:id,employer_id,country,state_id,township_id,address_detail', 'EmployerTestimonial:id,employer_id,name,title,remark,image','EmployerMedia:id,employer_id,name,type'])->whereId($employer->id)->select('id','logo','background','name','industry_id','ownership_type_id','type_of_employer','phone','website','no_of_offices','no_of_employees','legal_docs','summary','value', 'package_id')->first();
         $ownershipTypes = OwnershipType::whereNull('deleted_at')->get();
-        $functional_areas = FunctionalArea::whereNull('deleted_at')->whereFunctionalAreaId(0)->whereIsActive(1)->get();
         $type_of_employers = config('typeOfEmployer.value');
         return response()->json([
             'status' => 'success',
             'account_info' => $account_info,
             'employer' => $employer,
-            'type_of_employers' => $type_of_employers
+            'type_of_employers' => $type_of_employers,
+            'ownershipTypes' => $ownershipTypes
         ], 200);
     }
 
