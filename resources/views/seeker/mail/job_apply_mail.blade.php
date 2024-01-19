29c5885ffb285e068f9cf69144a7204cd350f187
chuukkhaing <chuukkhaing96@gmail.com>
2024-01-19 11:33:22 +0630

    apply-mail

--- resources/views/seeker/mail/job_apply_mail.blade.php
+++ resources/views/seeker/mail/job_apply_mail.blade.php
@@ -21,7 +21,7 @@
 
         @foreach($recommended_jobs as $job_post)
         <div style="width: 700px; margin: 20px 10px">
-            <a style="text-decoration: none" href="{{ route('jobpost-detail', $job_post->slug) }}')">
+            <a style="text-decoration: none" href="{{ env('MAIN_DOMAIN').'/job/'. $job_post->slug }}">
                 <h3 style="margin: 0;">{{ $job_post->job_title }}</h3>
                 <div style="width: 80%; position: relative; display: inline-block; vertical-align: top;">
                     <p>{!! \Illuminate\Support\Str::words(strip_tags($job_post->job_requirement), 25, $end = '...') !!}</p>
@@ -31,7 +31,7 @@
                     @php 
                     $s3 = Illuminate\Support\Facades\Storage::disk('s3');
                     $client = $s3->getDriver()->getAdapter()->getClient();
-                    $expiry = "+10 minutes";
+                    $expiry = now()."+ 7 days";
                     if(isset($jobApply->Employer->employer_id)) {
                         $command = $client->getCommand('GetObject', [
                             'Bucket' => env('AWS_BUCKET'),
@@ -51,7 +51,7 @@
                     <img src="{{ $url }}" alt="Profile Image" style="width: 75px;">
                     
                 </div>
-                <a href="{{ route('jobpost-detail', $job_post->slug) }}" style="text-decoration: none; padding: 10px 50px; background: #0355D0; color: #FFD200; border-radius: 50px; box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.2); font-weight: bold;">View</a>
+                <a href="{{ env('MAIN_DOMAIN').'/job/'. $job_post->slug }}" style="text-decoration: none; padding: 10px 50px; background: #0355D0; color: #FFD200; border-radius: 50px; box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.2); font-weight: bold;">View</a>
             </a>
             <hr style="margin: 20px 0;">
         </div>
