c5fc9b7447ef32d7682dfb3bfe540c6fe05d9988
chuukkhaing <chuukkhaing96@gmail.com>
2024-02-01 16:34:37 +0630

    email-template

--- resources/views/seeker/verify/verifyEmail.blade.php
+++ resources/views/seeker/verify/verifyEmail.blade.php
@@ -30,28 +30,30 @@
 </head>
     <body>
         <div style="margin: auto;" class="verify-template">
-            <div style="background-image: url({{ asset('/img/background/EMAIL_ACTIVATE_Recovered.jpg') }}); background-position: center; background-repeat: no-repeat; background-size: cover; ">
-                <div style="text-align: center;" class="activate-title">
-                    <h2 style="font-weight: 900; color: #0355D0">ACTIVATE</h2>
-                    <h4>Your Infinity Careers Account</h4>
-                    <div>
+            <table style="background-image: url({{ asset('/img/background/EMAIL_ACTIVATE_Recovered.jpg') }}); background-position: center; background-repeat: no-repeat; background-size: cover; ">
+                <tbody style="text-align: center;">
+                    <tr class="activate-title">
+                        <h2 style="font-weight: 900; color: #0355D0">ACTIVATE</h2>
+                        <h4>Your Infinity Careers Account</h4>
+                    </tr>
+                    <tr>
                         <p style="color: #0355D0; font-weight: 600;">Hi {{ $seeker->email }},</p>
                         <p style="color: #0355D0; font-weight: 600;">Welcome to Infinity Careers - Where Opportunities Await!</p>
                         <p style="color: #0355D0; font-weight: 600;">To start your job search journey, simply click <br> the button below to activate your account:</p>
-                    </div>
-                    <div style="margin: 20px 0;">
-                        <div style="background-image: url({{ asset('/img/background/a.png') }}); background-size: contain; background-repeat: no-repeat; background-position: center;" class="middle-image">
-                        </div>
-                    </div>
-                    <div style="margin: 20px 0;">
+                    </tr>
+                    <tr style="margin: 20px 0;">
+                        <tr style="background-image: url({{ asset('/img/background/a.png') }}); background-size: contain; background-repeat: no-repeat; background-position: center;" class="middle-image">
+                        </tr>
+                    </tr>
+                    <tr style="margin: 20px 0;">
                         <a href="{{ route('seeker-verify',$seeker->email_verification_token) }}" style="text-decoration: none; padding: 10px 50px; background: #0355D0; color: #FFD200; border-radius: 50px; box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.2); font-weight: bold;">Activate</a>
-                    </div>
-                    <div style="margin: 20px 0;">
+                    </tr>
+                    <tr style="margin: 20px 0;">
                         <p style="color: #0355D0; font-weight: 600;">With Infinity Careers, you'll discover exciting job opportunities and resources <br> to boost your career. We're here to help you succeed.</p>
                         <p style="color: #0355D0; font-weight: 600;">If you have any questions, our support team is ready to assist you.</p>
-                    </div>
+                    </tr>
                     
-                    <div style="margin: 20px 0;">
+                    <tr style="margin: 20px 0;">
                         <p style="color: #fff; font-weight: 600;">Thanks for choosing us!</p>
                         <a href="https://www.facebook.com/infinitycareersmyanmar2021"><img src="{{ asset('/img/icon/facebook.png') }}" alt="" style="width: 25px;"></a>
                         <a href="https://invite.viber.com/?g2=AQBfOlaPXsJ6208t76pHaWT%2FqlOO%2BD4G6B9nQbRfU2UrK1C4KRstKkWJGBTjsffm"><img src="{{ asset('/img/icon/viber.png') }}" alt="" style="width: 25px;"></a>
@@ -59,9 +61,9 @@
                         <a href="https://www.linkedin.com/company/infinitycareersmyanmar/"><img src="{{ asset('/img/icon/icon_Linkedin.png') }}" alt="" style="width: 25px;"></a>
                         <p style="color: #fff; font-weight: 600;">Best Regards,</p>
                         <p style="color: #fff; font-weight: 600;">The Infinity Careers Team</p>
-                    </div>
-                </div>
-            </div>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </body>
 </html>
