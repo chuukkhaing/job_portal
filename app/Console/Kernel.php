abdb07a1011239c1f514ca80bd13506c067de505
chuukkhaing <chuukkhaing96@gmail.com>
2024-06-03 16:33:42 +0630

    job-post-expire

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('job_posts:expire')->dailyAt('00:00')->withoutOverlapping(10);;
+        $schedule->command('job_posts:expire')->dailyAt('00:00');
     }
 
     /**
